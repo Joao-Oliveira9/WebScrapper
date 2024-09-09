8b16c21ec603465ca09278ba4e55a9af5b596436
Igor <igormatosgaeta@gmail.com>
2024-09-09 16:43:28 -0300

    testes do puppeteer

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("teste");
+const puppeteer = require('puppeteer');
+async function urubupix() {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const URL = "https://www.youtube.com/watch?v=jDI1Z2PjOa0";
+    // Navigate the page to a URL
+    await page.goto(URL);
+    await page.waitForSelector('yt-formatted-string.style-scope.ytd-watch-metadata', { timeout: 5_000 });
+    const titulo = await page.evaluate(() => {
+        return document.querySelector('yt-formatted-string.style-scope.ytd-watch-metadata').innerText;
+    });
+    console.log('The title of this blog post is "%s".', titulo);
+    await browser.close();
+}
+urubupix();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("teste");
+const puppeteer = require('puppeteer');
+
+async function urubupix () {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    
+    const URL = "https://www.youtube.com/watch?v=jDI1Z2PjOa0";
+
+    // Navigate the page to a URL
+    await page.goto(URL);
+    await page.waitForSelector('yt-formatted-string.style-scope.ytd-watch-metadata', { timeout: 5_000 });
+    
+    const titulo = await page.evaluate(() => {
+        return (document.querySelector('yt-formatted-string.style-scope.ytd-watch-metadata') as HTMLElement).innerText
+    });
+
+    console.log('The title of this blog post is "%s".', titulo);
+  
+    await browser.close();
+  }
+
+  urubupix();
