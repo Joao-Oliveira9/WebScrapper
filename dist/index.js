27865332376ed5fbb8fe66d07391eb2bf16621df
Joao-Oliveira9 <joaoliveira9@outlook.com>
2024-09-09 16:43:40 -0300

    Update: acessing a web-page and take a title element

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("teste");
+const puppeteer_1 = __importDefault(require("puppeteer"));
+async function teste() {
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer_1.default.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/watch?v=4W55nFDyIrc&t=82s");
+    await page.waitForSelector("yt-formatted-string.style-scope.ytd-watch-metadata", { timeout: 5_000 });
+    const title = await page.evaluate(() => {
+        return document.querySelector("yt-formatted-string.style-scope ytd-watch-metadata").innerText;
+    });
+    console.log(title);
+    await browser.close();
+}
+teste();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("teste");
+import puppeteer from "puppeteer";
+
+async function teste() {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.youtube.com/watch?v=4W55nFDyIrc&t=82s");
+
+  await page.waitForSelector(
+    "yt-formatted-string.style-scope.ytd-watch-metadata",
+    { timeout: 5_000 }
+  );
+  const title = await page.evaluate(() => {
+    return (
+      document.querySelector(
+        "yt-formatted-string.style-scope.ytd-watch-metadata"
+      ) as HTMLElement
+    ).innerText;
+  });
+  console.log(title);
+  await browser.close();
+}
+
+teste();
